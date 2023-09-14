fff0f945c8be04eafb168c82ede9bf80d664471f
Mayank Gupta <86046851+CMTCL-IITM@users.noreply.github.com>
2023-09-14 18:09:30 +0530

    Update TB_Hamiltonian.m

--- TB_Hamiltonian.m
+++ TB_Hamiltonian.m
@@ -22,7 +22,7 @@ f4 = 2*tpp_s*cos(kx*a)+2*tpp_p*cos(ky*a);
 f5 = 2*tpp_s*cos(ky*a)+2*tpp_p*cos(kx*a);
 f8 = 2*tpp_p*cos(kx*a)+2*tpp_p*cos(ky*a);
 
-%% Hamiltonian intralayer
+%% Hamiltonian intralayer (Hamiltonian for jth layer of the slab. See Section IV(C) of PRB,101,155102(2020))
 
 HTB1 = [Es+f0, f1, f2, 0,     0,  0,0,0;
        -f1,  Ep+f4, 0, 0,     0,  0,0,0;
@@ -34,7 +34,7 @@ HTB1 = [Es+f0, f1, f2, 0,     0,  0,0,0;
         0, 0,0,0,           -f2,    0,Ep+f5,0;
         0, 0,0,0,             0,    0,0,Ep+f8];
 
-%% Hamiltonian intralayer
+%% Hamiltonian intralayer (Hamiltonian for interaction between layer j and j − 1)
 
 HTB2 = [tss_s, 0,0,tsp_s,    0,  0,0,0;
         0, tpp_p,0,0,        0,  0,0,0;
